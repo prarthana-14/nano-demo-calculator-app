b84a3c648029bbe8e264fb966e4c1864ac43dd61
prarthana-14 <pnk140702@gmail.com>
2023-08-21 21:54:03 +0530

    add & sub

--- server.py
+++ server.py
@@ -1,19 +1,23 @@
-from flask import Flask
+from flask import Flask,request, make_response,jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Hello World"
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    first=request.args.get('first')
+    second=request.args.get('second')
+    return jsonify(result=int(first)+ int (second))
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    first=request.args.get('first')
+    second=request.args.get('second')
+    return jsonify(result=int(first)- int (second))
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080,host='127.0.0.1')
